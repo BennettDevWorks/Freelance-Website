8ad61e529758ec362ad923e0db8ee21b788502a3
ChristBennett909 <chrisjwbennett@gmail.com>
2026-02-05 18:41:03 +0000

    Adding specific offer description and pricing to .js file

--- Scripts/prepackaged.js
+++ Scripts/prepackaged.js
@@ -3,38 +3,65 @@ const offerGrid = document.getElementById('offerGrid');
 const toggleLabel = document.getElementById('toggleLabel');
 
 
-const offerSet1 = [
-	"Basic Offer",
-	"Premium Offer",
-	"FlagShip Offer"
+const websiteOfferSet= [
+	{
+		title: "Basic", 
+		price: "£500", 
+		description: "Getting yourself out there<br>A splash website getting yourself out there and telling the world who you are and how to contact you with your own personal design."},
+	{
+		title: "Premium",
+		price: "£1,000",
+		description: "A main website with more complexities like a shop for your products or services but limits on the amount of products in the shop."
+	},
+	{
+		title: "FlagShip",
+		price: "£2,000",
+		description: "Your flagship website showing you own your industry not just work in it, with all your products and services on show for the purchase and all the pages you could need for you and your customers."
+	}
 ];
 
-const offerSet2 = [
-	"Basic Offer",
-	"Premium Offer",
-	"FlagShip Offer"
+const appOfferSet = [
+	{
+		title: "Basic",
+		price: "From £4,000",
+		description: "Your very own custom app another pillar to your ecosystem so your customers know you have them at every turn.(On going maintenance cost: £15/month)"
+	},
+	{
+		title: "Premium",
+		price: "From £8,000",
+		description: "Putting some of those turns inside the same box with API's, more pages, more complexity taken away to give you and you customers simplicity.(On going maintenance cost: £15/month)"
+	},
+	{
+		title: "FlagShip",
+		price: "From £12,00",
+		description: "We'll put all the turns in one box and put a pretty bow on top that matches your design and style, your one stop shop, your ecosystem held in one hand accesssible in seconds.(On goin maintenance cost: £20/month)"
+	}
 ];
 
 
 function renderOffers(offers){
-	offersGrid.innerHTML = "";
+	offerGrid.innerHTML = "";
 	offers.forEach(offer => {
 		const div = document.createElement('div');
 		div.className = 'grid-item';
-		div.textContent = offer;
-		offersGrid.appendChild(div);
+		div.innerHTML = `
+			<h3>${offer.title}</h3>
+			<p><strong>${offer.price}</strong></p>
+			<p>${offer.description}</p>
+		`;
+		offerGrid.appendChild(div);
 	});
 }
 
 
-renderOffers(offerSet1);
+renderOffers(websiteOfferSet);
 
 offerToggle.addEventListener('change', () => {
 	if (offerToggle.checked){
-		renderOffers(offerSet2);
-		toggleLabel.textContent = "Website Offers";
+		renderOffers(appOfferSet);
+		toggleLabel.textContent = "App Offers";
 	} else {
-		renderOffers(offerSet1);
-		toggleLabel.textContent = "App offers";
+		renderOffers(websiteOfferSet);
+		toggleLabel.textContent = "Website Offers";
 	}
 });
